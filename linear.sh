bc8e9f7b7c65ccd0a3ac28c56639d0d5006fdbb3
mauricio <mau.altgr@hotmail.com>
2022-01-21 16:38:15 -0300

    add asas and linear datasets

--- linear.sh
+++ linear.sh
@@ -3,8 +3,8 @@
 declare -i gpu=2
 declare -i epochs=2
 
-python main_od.py --dataset linear --device $gpu --oc 0 --e $epochs
-python main_od.py --dataset linear --device $gpu --oc 1 --e $epochs
-python main_od.py --dataset linear --device $gpu --oc 2 --e $epochs
-python main_od.py --dataset linear --device $gpu --oc 3 --e $epochs
-python main_od.py --dataset linear --device $gpu --oc 4 --e $epochs
+python main_od.py --dataset linear --device cuda:$gpu --oc 0 --e $epochs
+python main_od.py --dataset linear --device cuda:$gpu --oc 1 --e $epochs
+python main_od.py --dataset linear --device cuda:$gpu --oc 2 --e $epochs
+python main_od.py --dataset linear --device cuda:$gpu --oc 3 --e $epochs
+python main_od.py --dataset linear --device cuda:$gpu --oc 4 --e $epochs
