8e596258a4f7b9667ef265be9b36001b81776ee9
mauricio <mau.altgr@hotmail.com>
2022-02-04 10:56:50 -0300

    add final jobs

--- ztf.sh
+++ ztf.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 declare -i gpu=1
-declare -i epochs=2
+declare -i epochs=2048
 
 python main_od.py --dataset ztf_transient --device cuda:$gpu --oc 0 --e $epochs
 python main_od.py --dataset ztf_stochastic --device cuda:$gpu --oc 0 --e $epochs
